05cedb73583210cd22ee2547049fd070b257d59d
Phillip Huang <phillip96huang@gmail.com>
2019-10-29 16:49:03 -0400

    Complete ActiveRecord Associations

--- 1_active_record_lite/lib/03_associatable.rb
+++ 1_active_record_lite/lib/03_associatable.rb
@@ -10,41 +10,68 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    self.model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @class_name = options[:class_name]
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+
+    @class_name ||= name.to_s.camelize
+    @foreign_key ||= "#{name}_id".intern
+    @primary_key ||= :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @class_name = options[:class_name]
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+
+    @class_name ||= name.to_s.singularize.camelize
+    @foreign_key ||= "#{self_class_name.underscore}_id".intern
+    @primary_key ||= :id
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    self.assoc_options[name] = options
+    define_method(name) do 
+      f_key, p_key = options.foreign_key, options.primary_key
+      id = self.send(f_key)
+      model_class = options.model_class
+      matching = model_class.where(id: id)
+      matching.first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+    define_method(name) do 
+      f_key, p_key = options.foreign_key, options.primary_key
+      id = self.send(p_key)
+      model_class = options.model_class
+      model_class.where(f_key => id)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
--- 1_active_record_lite/lib/04_associatable2.rb
+++ 1_active_record_lite/lib/04_associatable2.rb
@@ -5,6 +5,36 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    through_options = self.assoc_options[through_name]
+
+    define_method(name) do
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      source_table = source_options.table_name
+
+      f_key = through_options.foreign_key
+      p_key = through_options.primary_key
+      through_id = self.send(f_key)
+
+      join_f_key = source_options.foreign_key
+      join_p_key = source_options.primary_key
+      
+      query = <<-SQL
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table} ON #{through_table}.#{join_f_key}
+                            = #{source_table}.#{join_p_key}
+        WHERE
+          #{through_table}.#{p_key} = ?
+      SQL
+
+      rows = DBConnection.execute(query, through_id)
+      source_options.model_class.new(rows.first)
+    end
+
   end
 end
